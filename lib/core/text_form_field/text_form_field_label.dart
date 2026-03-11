819e199a61b59f8ff5f4ac05f8ed18a64a26bb6b
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-03-11 10:28:54 +0900

    style: TextFormField 추가

---
+++ lib/core/text_form_field/text_form_field_label.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:nsg_mobile/constants/text_style.dart';
+
+class CustomTextFormFieldLabel extends StatelessWidget {
+  final Widget? label;
+  final String? labelText;
+  final TextStyle? labelStyle;
+
+  const CustomTextFormFieldLabel({
+    super.key,
+    this.label,
+    this.labelText,
+    this.labelStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final defaultTextStyle = NsgTextStyle.body3;
+    return DefaultTextStyle(
+      style: defaultTextStyle.merge(labelStyle),
+      child: Row(children: [label ?? Text(labelText!)]),
+    );
+  }
+}
