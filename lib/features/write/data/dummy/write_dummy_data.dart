5bb7f3b35a235b2208ff5e29f6b978d2a197eac9
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-03-23 16:14:19 +0900

    feat: 각 카테고리별 사진 추가

--- lib/features/write/data/dummy/write_dummy_data.dart
+++ lib/features/write/data/dummy/write_dummy_data.dart
@@ -6,25 +6,30 @@ const writeCategories = [
     label: '장소\n공유하기',
     icon: Symbols.person_pin,
     route: '/write/place',
+    imagePath: 'assets/svg/place_write.png',
   ),
   WriteCategory(
     label: '기숙사 꿀팁\n공유하기',
     icon: Symbols.bed,
     route: '/write/dormitory',
+    imagePath: 'assets/svg/dorm_write.png',
   ),
   WriteCategory(
     label: '대마고 꿀팁\n공유하기',
     icon: Symbols.school,
     route: '/write/school',
+    imagePath: 'assets/svg/school_write.png',
   ),
   WriteCategory(
     label: '기타 꿀팁\n공유하기',
     icon: Symbols.local_fire_department,
     route: '/write/etc',
+    imagePath: 'assets/svg/etc_write.png',
   ),
   WriteCategory(
     label: '전공 꿀팁\n공유하기',
     icon: Symbols.laptop_mac,
     route: '/write/major',
+    imagePath: 'assets/svg/major_write.png',
   ),
 ];
