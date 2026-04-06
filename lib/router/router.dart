0f2ac4d6949a3a5a03c264ab8c1b08c2f533e8b3
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-04-06 09:45:04 +0900

    feat: 상태관리 및 경로 추가

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:nsg_mobile/core/layout/main_layout.dart';
 import 'package:nsg_mobile/features/auth/presentation/screens/login_screen.dart';
 import 'package:nsg_mobile/features/auth/presentation/screens/splash_screen.dart';
+import 'package:nsg_mobile/features/major/presentation/screens/major_post_list_screen.dart';
 import 'package:nsg_mobile/features/major/presentation/screens/major_screen.dart';
 import 'package:nsg_mobile/features/map/presentation/screens/map_screen.dart';
 import 'package:nsg_mobile/features/mypage/presentation/screens/mypage_screen.dart';
@@ -93,6 +94,20 @@ final router = GoRouter(
             GoRoute(
               path: '/major',
               builder: (context, state) => const MajorScreen(),
+              routes: [
+                GoRoute(
+                  path: 'popular',
+                  builder: (context, state) => const MajorPostListScreen(
+                    type: MajorPostListType.popular,
+                  ),
+                ),
+                GoRoute(
+                  path: 'recent',
+                  builder: (context, state) => const MajorPostListScreen(
+                    type: MajorPostListType.recent,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
